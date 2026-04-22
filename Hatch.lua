714ddace87397f768a9e32bdd09c1ab8b717a19c
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-22 18:58:39 +0700

    Create Hatch.lua

---
+++ Hatch.lua
@@ -0,0 +1,20 @@
+task.wait(35)
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remote = ReplicatedStorage:WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient")
+local THREADS = 30
+local DELAY = 0.02 
+local function hatch()
+    remote:InvokeServer("EasterHatchEvent", "HatchRequest")
+    if math.random() < 0.125 then
+        remote:InvokeServer("EasterHatchEvent", "HatchRequest", math.random(1, 3))
+    end
+end
+for i = 1, THREADS do
+    task.spawn(function()
+        while true do
+            hatch()
+            task.wait(DELAY)
+        end
+    end)
+end
