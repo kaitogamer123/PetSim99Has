a9b498f018edbbe5484ac843ae0fd43d51bef95f
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-22 18:25:50 +0700

    Update BestFarm.lua

--- BestFarm.lua
+++ BestFarm.lua
@@ -1,3 +1,4 @@
+print("injected")
 -- Ждем загрузку игры
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character
@@ -407,3 +408,21 @@ task.spawn(function()
         end)
     end
 end)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remote = ReplicatedStorage:WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient")
+local THREADS = 30
+local DELAY = 0.02 
+local function hatch()
+    remote:InvokeServer("EasterHatchEvent", "HatchRequest")
+    if math.random() < 0.125 then
+        remote:InvokeServer("EasterHatchEvent", "HatchRequest", math.random(1, 3))
+    end
+end
+for i = 1, THREADS do
+    task.spawn(function()
+        while true do
+            hatch()
+            task.wait(DELAY)
+        end
+    end)
+end
