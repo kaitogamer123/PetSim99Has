f7ae8fd3381a89a12d45aae09b372981dab2af8f
kaitogamer123 <140911276+kaitogamer123@users.noreply.github.com>
2026-04-27 21:22:33 +0700

    Create Autopass.lua

---
+++ Autopass.lua
@@ -0,0 +1,23 @@
+
+
+task.spawn(function()
+    while true do
+        if _G.AutoFarmEnabled then
+            -- Если автофарм еще не включен - включаем его официально
+            if not AutoFarmCmds.IsEnabled() then
+                pcall(function()
+                    AutoFarmCmds.Enable()
+                end)
+            end
+        else
+            -- Если ты выключил чит - выключаем и автофарм игры
+            if AutoFarmCmds.IsEnabled() then
+                pcall(function()
+                    AutoFarmCmds.Disable()
+                end)
+            end
+        end
+        task.wait(1) -- Проверяем статус раз в секунду
+    end
+end)
+
